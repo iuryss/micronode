0dc5a47712767457a35d9a4f68e5be3a57845ca5
Iury <iuryrbs@edu.unifor.br>
2024-12-01 20:19:52 -0300

    alteracoes

--- src/models/funcionario.js
+++ src/models/funcionario.js
@@ -2,8 +2,8 @@ import pool from '../config/database.js';
 
 export const createFuncionario = async (funcionario) => {
     const [result] = await pool.query(
-    'INSERT INTO funcionario (nome, telefone, endereco, salario, profissao) VALUES (?, ?, ?, ?, ?)',
-    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.salario, funcionario.profissao]
+    'INSERT INTO funcionario (nome, telefone, endereco, profissao, salario) VALUES (?, ?, ?, ?, ?)',
+    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.profissao, funcionario.salario]
     );
     return result;
 };
@@ -14,19 +14,19 @@ export const getFuncionarios = async () => {
 };
 
 export const getFuncionarioById = async (id) => {
-  const [rows] = await pool.query('SELECT * FROM funcionario WHERE id_funcionario = ?', [id]);
+  const [rows] = await pool.query('SELECT * FROM funcionario WHERE id = ?', [id]);
     return rows[0];
 };
 
 export const updateFuncionario = async (id, funcionario) => {
     const [result] = await pool.query(
-    'UPDATE funcionario SET nome = ?, telefone = ?, endereco = ?, salario = ?, profissao = ? WHERE id_funcionario = ?',
-    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.salario, funcionario.profissao, id]
+    'UPDATE funcionario SET nome = ?, telefone = ?, endereco = ?, profissao = ?, salario = ? WHERE id = ?',
+    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.profissao, funcionario.salario, id]
     );
     return result;
 };
 
 export const deleteFuncionario = async (id) => {
-    const [result] = await pool.query('DELETE FROM funcionario WHERE id_funcionario = ?', [id]);
+    const [result] = await pool.query('DELETE FROM funcionario WHERE id = ?', [id]);
     return result;
 };

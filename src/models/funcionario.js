44d3bd65f5072d3788a655d230112e376565f7df
Iury <iuryrbs@edu.unifor.br>
2024-12-02 08:50:11 -0300

    ajustesfinais

--- src/models/funcionario.js
+++ src/models/funcionario.js
@@ -1,9 +1,11 @@
+import axios from 'axios';
 import pool from '../config/database.js';
 
 export const createFuncionario = async (funcionario) => {
+    const eventoID =  await fetchEvento();
     const [result] = await pool.query(
-    'INSERT INTO funcionario (nome, telefone, endereco, profissao, salario) VALUES (?, ?, ?, ?, ?)',
-    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.profissao, funcionario.salario]
+    'INSERT INTO funcionario (nome, telefone, endereco, profissao, salario, eventoID) VALUES (?, ?, ?, ?, ?, ?)',
+    [funcionario.nome, funcionario.telefone, funcionario.endereco, funcionario.profissao, funcionario.salario, eventoID]
     );
     return result;
 };
@@ -35,4 +37,19 @@ export const getProfessores = async () => {
     const [result] = await pool.query("SELECT * FROM funcionario WHERE profissao = ?", ["PROFESSOR"]);
     return result;
 };
+
+const fetchEvento = async () => {
+    try {
+        const response = await axios.get('https://av3ronaldo-production.up.railway.app/eventos');
+        const eventos = response.data;
+        if (eventos.length === 0) {
+           return null;
+        }
+        const eventoAleatorio = eventos[Math.floor(Math.random() * eventos.length)];
+        return eventoAleatorio.id;
+    } catch (error) {
+        console.error('Erro ao buscar eventos:', error.message);
+        throw new Error('Não foi possível buscar eventos.');
+    }
+}
 
